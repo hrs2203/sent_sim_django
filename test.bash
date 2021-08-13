f4e2afb87a5394850e8145da77a609a2f5fdbc2f
hrs2203 <hrishabh2203@gmail.com>
2021-08-13 11:00:36 +0530

    issue resolved with pylance

---
+++ test.bash
@@ -0,0 +1,2 @@
+python3 ~/Desktop/GitHub/sent_sim_django/similarity_backend/comparision_engine/corpus_based/sent_sim.py
+python3 ~/Desktop/GitHub/sent_sim_django/similarity_backend/comparision_engine/sent_bert/samp1.py
