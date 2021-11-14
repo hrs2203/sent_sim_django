f4659f793db6331d820a87df7e76172daf489d8b
hrs2203 <hrishabh@gmail.com>
2021-11-14 12:01:28 +0530

    ipynb updated

--- similarity_backend/user_component/comparision_engine/data/read_tsv.py
+++ similarity_backend/user_component/comparision_engine/data/read_tsv.py
@@ -4,3 +4,14 @@ def read_file(fileName: str, sep: str = "\t"):
         fileContent = fileObject.read().split("\n")
         fileParsedContent = [item.split(sep) for item in fileContent]
     return fileParsedContent
+
+def read_quora_data(fileName: str, sep: str = " \",\" "):
+    fileContent = None
+    with open(fileName, 'r') as fileObject:
+        fileContent = fileObject.read().split("\n")
+        fileParsedContent = []
+        for item in fileContent:
+            temp = item.split(sep) 
+            if len(temp) == 6:
+                fileContent.append( fileContent[-1][1:-1], fileContent[-3][1:-1], fileContent[-2][1:-1] )
+    return fileParsedContent[1:]
