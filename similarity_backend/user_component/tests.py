05520539607b40a4035c677e650ed0915caea4df
hrs2203 <hrishabh@gmail.com>
2021-11-15 09:49:22 +0530

    final pdf, added 12

--- similarity_backend/user_component/tests.py
+++ similarity_backend/user_component/tests.py
@@ -1,18 +1,12 @@
 from django.test import TestCase
 
-from comparision_engine import MultiSentenceBertComparision, CorpusBasedComparision
+from comparision_engine import MultiSentenceBertComparision
 
-la = [
-    CorpusBasedComparision("Death bed","Nice time"),
-    CorpusBasedComparision("Death bed","good time"),
-    CorpusBasedComparision("Death bed","Nice Day"),
-    CorpusBasedComparision("Death bed","grave yard")
-]
 
 lb = MultiSentenceBertComparision( ["Death bed"], ["Nice time","good time","Nice day","grave yard"])
 
 
-for i in zip(la, lb[0]):
+for i in zip(lb[0]):
     print(i)
 
 
